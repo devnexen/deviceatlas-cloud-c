8839635ca8186b10367c3f6da23e172f5a42e33f
David Carlier <devnexen@gmail.com>
2015-09-17 15:17:24 +0100

    various fixes

--- redis_cache_provider.c
+++ redis_cache_provider.c
@@ -8,10 +8,18 @@
 
 static int
 redis_cache_setunixsock(void **cache_obj, char *cfg_str) {
-    char *sockpath = strchr(cfg_str, ':');
-    if (sockpath != NULL && ++ *sockpath != 0) {
-        *cache_obj = redisConnectUnix(sockpath);
-        return (*cache_obj != NULL ? 0 : -1);
+    char *sockpath = strchr(cfg_str, ' ');
+    if (sockpath != NULL) {
+        redisContext *ctx;
+        while (isspace(*sockpath) && sockpath++);
+        ctx = redisConnectUnix(sockpath);
+        if (ctx == NULL) {
+            fprintf(stderr, "could not set '%s' unix socket path\n", sockpath);
+            return (-1);
+        }
+
+        *cache_obj = ctx;
+        return (0);
     }
 
     return (-1);
@@ -19,7 +27,8 @@ redis_cache_setunixsock(void **cache_obj, char *cfg_str) {
 
 static int
 redis_cache_settcp(void **cache_obj, char *cfg_str) {
-    char *strport = strchr(cfg_str, ':');
+    redisContext *ctx;
+    char *strport = strchr(cfg_str, ' ');
     int port = 6379;
     if (strport != NULL && *(strport + 1) != 0) {
         int tmp;
@@ -31,19 +40,24 @@ redis_cache_settcp(void **cache_obj, char *cfg_str) {
         cfg_str[pos] = 0;
     }
     
-    *cache_obj = redisConnect(cfg_str, port);
-    return (*cache_obj != NULL ? 0 : -1);
+    ctx = redisConnect(cfg_str, port);
+    if (ctx == NULL) {
+        fprintf(stderr, "could not set to '%s' host, port '%d'", cfg_str, port);
+        return (-1);
+    }
+
+    *cache_obj = ctx;
+    return (0);
 }
 
 int
 redis_cache_init(struct da_cloud_cache_cfg *cfg) {
     char *cfg_str = cfg->cache_cfg_str;
     cfg->cache_obj = NULL;
-    if (strncasecmp(cfg_str, "unix", 4) == 0 && strlen(cfg_str) > 5) {
+    if (strncasecmp(cfg_str, "unixsocket", 10) == 0)
         return (redis_cache_setunixsock(&cfg->cache_obj, cfg_str));
-    } else {
+    else
         return (redis_cache_settcp(&cfg->cache_obj, cfg_str));
-    }
 }
 
 int
@@ -54,9 +68,10 @@ redis_cache_get(struct da_cloud_cache_cfg *cfg, const char *key, char **value) {
         if (ret != NULL) {
             if (ret->str != NULL)
                 *value = strdup(ret->str);
-	    freeReplyObject(ret);
+            freeReplyObject(ret);
         }
     }
+
     return (-1);
 }
 
@@ -68,8 +83,11 @@ redis_cache_set(struct da_cloud_cache_cfg *cfg, const char *key, const char *val
         if (ret != NULL) {
             freeReplyObject(ret);
             return (0);
+        } else {
+            fprintf(stderr, "could not set cache: %s\n", ((redisContext *)cfg->cache_obj)->errstr);
         }
     }
+
     return (-1);
 }
 
