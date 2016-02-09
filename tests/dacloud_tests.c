e3294bf10b6c7f82f2d8bd3dccb4e12d555366e6
David Carlier <devnexen@gmail.com>
2016-02-09 11:11:40 +0000

    additional unit test for clientside + addition mode for unit tests

--- tests/dacloud_tests.c
+++ tests/dacloud_tests.c
@@ -1,5 +1,6 @@
 #include <CUnit/CUnit.h>
 #include <CUnit/Automated.h>
+#include <CUnit/Basic.h>
 
 #include <dacloud.h>
 #include <stdlib.h>
@@ -32,9 +33,18 @@ void
 da_cloud_simple_lookup(void) {
 	struct da_cloud_header_head headers;
 	struct da_cloud_property_head properties; 
+    struct da_cloud_property *p;
+    const char *property;
+
+    property = "model";
+
 	da_cloud_header_init(&headers);
 	da_cloud_header_add(&headers, "user-agent", "iPhone");
+    da_cloud_clientside_add(&headers, "sdevicePixelRatio:2|sdeviceAspectRatio:40/71");
 	CU_ASSERT_FATAL(da_cloud_detect(&config, &headers, &properties) == 0);
+    CU_ASSERT_FATAL(da_cloud_property(&properties, property, &p) == 0);
+    CU_ASSERT_STRING_EQUAL(p->name, property);
+    CU_ASSERT_STRING_EQUAL(p->value.s, "iPhone 5");
 	da_cloud_properties_free(&properties);
 	da_cloud_header_free(&headers);
 }
@@ -52,7 +62,7 @@ int
 main(int argc, char *argv[]) {
 	CU_ErrorCode cu;
 	CU_pSuite suite;
-	const char *configpath;
+	const char *configpath, *mode = NULL;
 	if (argc < 2) {
 		fprintf(stderr, "please gives a configuration file\n");
 		exit(-1);
@@ -60,13 +70,19 @@ main(int argc, char *argv[]) {
 
 	cu = CU_initialize_registry();
 	configpath = argv[1];
+    if (argc > 2)
+            mode = argv[2];
 
 	if (da_cloud_init(&config, configpath) == 0) {
 		suite = CU_add_suite("da_cloud", NULL, NULL);
 		CU_add_test(suite, "da_cloud_headers_test", da_cloud_headers_test);
 		CU_add_test(suite, "da_cloud_simple_lookup", da_cloud_simple_lookup);
 		CU_add_test(suite, "da_cloud_properties_test", da_cloud_properties_test);
-		CU_automated_run_tests();
+
+        if (mode != NULL && strcasecmp(mode, "show") == 0)
+                CU_basic_run_tests();
+        else
+                CU_automated_run_tests();
 	}
 	da_cloud_fini(&config);
 	CU_cleanup_registry();
