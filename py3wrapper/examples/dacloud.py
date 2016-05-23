0e50a65023dd97d2f2be818a59bf7c54728934a4
David Carlier <devnexen@gmail.com>
2016-05-23 23:00:40 +0100

    python example fix ...

--- py3wrapper/examples/dacloud.py
+++ py3wrapper/examples/dacloud.py
@@ -1,7 +1,7 @@
 from dacloud import DaCloud
 import sys
 
-if sys.argv.len == 3:
+if len(sys.argv) == 3:
     dc = DaCloud()
     if dc.load_config(sys.argv[1]):
         print(dc.detect({"user-agent":sys.argv[2]}))
